8b16bfe1af95f1ced8f15c05ed5f1ac826fc8355
elmucho05 <algertsuta@gmail.com>
2023-10-13 19:51:04 +0200

    add insertion sort in descending order

--- InsertionSort/reverse_insertion_sort.py
+++ InsertionSort/reverse_insertion_sort.py
@@ -3,7 +3,7 @@ def insertion_sort(A):
         key=A[j]
         i = j-1
         print(A, "  key=", key)
-        while i>=0 and A[i]>key:
+        while i>=0 and A[i]<key:
             A[i+1] = A[i]
             i = i-1
         A[i+1] = key
