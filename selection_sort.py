84b0f19f64240f92d1692c4351ab4b63485fa486
elmucho05 <algertsuta@gmail.com>
2023-11-05 20:53:56 +0100

    selection_sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -7,23 +7,30 @@ The login behind slection sort is the following:
 Selection sort is a Recursive procedure and not a fuction beacuse it does not return anything
 """
 
-def selection_sort(A,i,n):
-    pass
-
 def indexMin(A,i,n):
-    #say i have indexMin(A,0,len(A)-1)
+    #say i have indexMin(A,0, 5)
     index_of_min = i #the index of the min value is the first value initially
     
     #scrolling the next elements
-    for j in range(i+1, n-1):
+    for j in range(i+1, n):
         if A[j] < A[index_of_min]:
             index_of_min = j # i have found a smaller element so i save it's index 
     
     return index_of_min
 
+def selection_sort(A, i, n):
+    if i< n-1: # if i haven't finished chekcing all the elements
+        #keep chekcing and save and save the index of the smallest element for each turn
+        index_min_value = indexMin(A,i,n)
+        
+        #after finding the index of the min value, you
+        #swap A[min_value] with A[1], the A[2] .... A[i]
+        # so with A[i] which is the index that i'm using
+        A[i],A[index_min_value] = A[index_min_value],A[i]
+        return selection_sort(A,i+1, n)
 
 if __name__ == "__main__":
     A = [2,4,3,5,6,1]
     print(A)
-    selection_sort(A, 0, len(A)-1)
+    selection_sort(A, 0, len(A))
     print(A)
