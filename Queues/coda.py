f773442a22d4e3e6ecfc241b80dd081e54f4b8d6
elmucho05 <algertsuta@gmail.com>
2023-11-17 23:15:38 +0100

    coda inizialized

--- Queues/coda.py
+++ Queues/coda.py
@@ -4,11 +4,24 @@ class Coda:
     tail = 0
 
 def enqueue(q,x):
-    pass
+    #q.coda[q.tail] = x
+    q.coda.append(x)
+    if q.tail == len(q.coda):
+        q.tail = 1
+    else:
+        q.tail = q.tail+1
 
 def dequeue(q):
-    pass
-
+    x = q.head
+    if q.head == len(q.coda):
+        q.head = 1
+    else:
+        q.head = q.head +1
+    return x
 if __name__ == "__main__":
     coda = Coda()
+    print(coda.coda)
+    enqueue(coda, 1)
+    print(coda.coda)
+    print(dequeue(coda))
     
