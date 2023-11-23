4cad9bd0e54563d48b235bb2bf7a75ea35bbce2a
elmucho05 <algertsuta@gmail.com>
2023-11-23 22:16:06 +0100

    added insert methods and bfs implementation

--- TreeVisits/bfs.cpp
+++ TreeVisits/bfs.cpp
@@ -74,27 +74,70 @@ node* new_node(int x){
     n->right = nullptr;
     return n;
 }
+/*
+                0
+            1       2
+        3       6        4
+                    1        5
+
+*/
+
+void insert_left(node* t, node* n){
+    if(t == nullptr)
+        cout << "error, everything nullptr";
+    t->left = n;
+}
+void insert_right(node* t, node* n){
+    if(t == nullptr)
+        cout << "error";
+    t->right = n;
+}
+
+void bfs(tree* t){
+    if(t->root != nullptr){
+        queue* q = new_queue();
+        enqueue(q, t->root);
+        while(!is_empty_queue(q)){
+            node* e = dequeue(q);
+            cout << e->key << " ";
+            if(e->left != nullptr)
+                enqueue(q, e->left);
+            if(e->right != nullptr)
+                enqueue(q, e->right);
+        }
+    }
+}
 
 
 int main(){
     tree* t =  new tree;
-    t->root = nullptr;
-
-    queue* q = new_queue();
-
+    t->root = new_node(0);
     node* n1 = new_node(1);
     node* n2 = new_node(2);
     node* n3 = new_node(3);
+    node* n4 = new_node(4);
+    node* n5 = new_node(5);
+    node* n6 = new_node(6);
+    node* n7 = new_node(7);
 
-    enqueue(q, n1);
-    enqueue(q, n2);
+    insert_left(t->root, n1);
+    insert_left(n1, n3);
+    insert_right(t->root, n2);
+    insert_left(n2, n6);
+    insert_right(n2, n4);
+    insert_left(n4, n7);
+    insert_right(n4, n5);
+    cout << "Printing elements of the queue: ";
+    /*
+                0
+    1                    2
+3                   6            4
+                            7         5
 
-    while(!is_empty_queue(q)){
-        node* current_node = dequeue(q);
-        cout << current_node->key << " "; 
-    }
-
-
+bfs should print : 0 1 2 3 6 4 7 5
+*/
+    bfs(t);
+    
 
     return 0;
 }
